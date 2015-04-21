df98f32463b6024dcfdc9ccd4ba7501f7d7c8f53
stolbov <stolbov@informatica.ru>
2015-04-21 13:57:37 +0300

    crop image

--- index.js
+++ index.js
@@ -19,24 +19,53 @@ var rect = new Kinetic.Rect({
 });
 
 var imageObj = new Image();
+var image;
 imageObj.src = 'osm.png';
+
 imageObj.onload = function() {
-    var image = new Kinetic.Image({
+    image = new Kinetic.Image({
         x: 20,
         y: 50,
         image: imageObj,
-        // width: 100,
-        // height: 100
-        scale: 1000
+        width: 100,
+        height: 100,
+        fill: 'red',
+        // scale: {x: 0.2, y:0.2}
+    });
+    image.crop({
+        x: 0,
+        y: 0,
+        width: 100,
+        height: 100
     });
     layer.add(image);
     image.on('click', function (e) {
         console.log(image.getWidth());
     });
+
+    layer.draw();
+var tween = new Kinetic.Tween({
+        node: image, 
+        duration: 6,
+        // x: 400,
+        // y: 75,
+        // rotation: 360 * 5,
+        opacity: 1,
+        // strokeWidth: 6,
+        scaleX: 2.3,
+        scaleY: 2.3,
+        easing: Kinetic.Easings.Linear,
+        fillRed: 0,
+        fillGreen: 0,
+        fillBlue: 255
+      });
+
+// tween.play();
+
+
 };
 
 // layer.add(rect);
-
 stage.add(layer);
 
     /*
